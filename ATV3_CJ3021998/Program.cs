c861ce69fdc340b785dfff8b7743f90475eb25e1
CJ3021998 <CJ3021998@LAB01-PC16>
2024-03-06 08:59:17 -0300

    Adicionar arquivos de projeto.

---
+++ ATV3_CJ3021998/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System.ComponentModel.Design;
+
+float nt;
+
+Console.WriteLine("Dê um valor para a nota de certo aluno");
+nt = float.Parse(Console.ReadLine());
+if (nt >= 6) {
+    Console.WriteLine("O aluno está aprovado");
+}
+else
+{
+    Console.WriteLine("O aluno está reprovado");
+}
